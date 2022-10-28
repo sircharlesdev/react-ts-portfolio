8466eb71cbbe678c1588b16de11b4bad40f26579
sircharlesdev <46936720+sircharlesdev@users.noreply.github.com>
2022-10-28 05:29:14 -0300

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import './style.css';
+import SimpleContainer from './components/SimpleContainer';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+      <SimpleContainer/>
+    </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-portfolio
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-8uwwuq)
